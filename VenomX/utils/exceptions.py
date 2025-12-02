22eff65f176ed6ddbdb5ee04c62b20fb02d12fdf
VT7org <glitchnaxusofficial@gmail.com>
2025-12-02 14:37:06 +0530

    Update exceptions.py

--- VenomX/utils/exceptions.py
+++ VenomX/utils/exceptions.py
@@ -10,3 +10,16 @@ class AssistantErr(Exception):
 
 class UnableToFetchCarbon(Exception):
     pass
+
+def is_ignored_error(err: Union[Exception, BaseException]) -> bool:
+    """
+    Determine if the error should be skipped from full logging.
+    Matches:
+    - Exception type (if enabled)
+    - Known substrings in error message
+    """
+    if isinstance(err, IGNORED_EXCEPTION_CLASSES):
+        return True
+
+    err_str = str(err).lower()
+    return any(keyword.lower() in err_str for keyword in IGNORED_ERROR_KEYWORDS)
